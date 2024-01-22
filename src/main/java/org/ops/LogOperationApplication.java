749407b225e3676b7b8d14526187e82c886c9d2e
Tuananh4403 <90849367+Tuananh4403@users.noreply.github.com>
2024-01-22 22:07:03 +0700

    Initial commit

---
+++ src/main/java/org/ops/LogOperationApplication.java
@@ -0,0 +1,13 @@
+package org.ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogOperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogOperationApplication.class, args);
+    }
+
+}
